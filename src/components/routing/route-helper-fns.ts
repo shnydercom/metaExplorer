813c803253aea6cdfadc6ab991c456fc85489ee1
aJonathanSchneider <ajs.private@gmail.com>
2019-03-31 13:36:35 +0200

    routing bug fixed for ../-routes, itpts adjusted

--- src/components/routing/route-helper-fns.ts
+++ src/components/routing/route-helper-fns.ts
@@ -6,6 +6,8 @@ export function cleanRouteString(destination: string, routes: LDRouteProps) {
 	if (route.startsWith("/")) {//i.e attach to path/create sub-path
 		route = route.substring(1);
 		route = match.url.endsWith("/") ? match.url + route : `${match.url}/${route}`;
+	} else if (route.startsWith("..")) {
+		route = route = match.url.endsWith("/") ? match.url + route : `${match.url}/${route}`;
 	} else {
 		route = match.url.substr(0, match.url.lastIndexOf("/") + 1) + route;
 	}
@@ -13,7 +15,11 @@ export function cleanRouteString(destination: string, routes: LDRouteProps) {
 	let newRouteParts: string[] = [];
 	for (let i = 0; i < routeParts.length; i++) {
 		const element = routeParts[i];
-		if ((i < routeParts.length - 1 && routeParts[i + 1] !== '..' && element !== '..') || i === routeParts.length - 1) {
+		if (element === '..') {
+			newRouteParts.pop();
+			continue;
+		}
+		if ((i < routeParts.length - 1) || i === routeParts.length - 1) {
 			newRouteParts.push(element);
 		}
 	}
