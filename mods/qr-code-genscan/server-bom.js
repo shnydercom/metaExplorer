4db7524b160ce46e7bcb392fac79584a75de8b28
aJonathanSchneider <ajs.private@gmail.com>
2019-08-12 15:48:15 +0200

    first server-BOM: for qrcode-mod

---
+++ mods/qr-code-genscan/server-bom.js
@@ -0,0 +1,19 @@
+// a bill of materials so that our dev-server or express can pick it up
+
+//const path = require('path');
+
+const qrcodeGen = require.resolve('qrcode-generator/qrcode.js');
+const qrcodeScan = require.resolve('qr-scanner/qr-scanner.min.js');
+const qrcodeScanWorker = require.resolve('qr-scanner/qr-scanner-worker.min.js');
+
+module.exports = function(app) {
+  app.get('/lib/qrcode-generator@1.4.3.js', function (req, res) {
+    res.sendFile(qrcodeGen);
+	});
+	app.get('/lib/qr-scanner@1.1.1.js', function (req, res) {
+    res.sendFile(qrcodeScan);
+	});
+	app.get('/lib/qr-scanner-worker.min.js@1.1.1.js', function (req, res) {
+    res.sendFile(qrcodeScanWorker);
+  });
+};
--- packages/metaexplorer.io/src/setupProxy.js
+++ packages/metaexplorer.io/src/setupProxy.js
@@ -1,11 +1,13 @@
 const proxy = require('http-proxy-middleware');
-const path = require('path');
-const other = require('@metaexplorer-mods/onboarding/src/onboardingEditor.json');
+//const path = require('path');
+const mxpIOItpts = require('@metaexplorer-nocode/metaexplorer.io/lib/interpreters.json');
+const qrCodeGenScanMod = require('@metaexplorer-mods/qr-code-genscan/server-bom.js');
 
 module.exports = function(app) {
   app.use(proxy('/interpreters', { target: 'http://localhost:7000/' }));
   app.get('/static/interpreters.json', function (req, res) {
-    res.send(other);
+    res.send(mxpIOItpts);
     //res.sendFile(path.join(__dirname + '/static/interpreters.json'));
   });
+  qrCodeGenScanMod(app);
 };
