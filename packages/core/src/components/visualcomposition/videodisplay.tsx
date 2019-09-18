55fda041ca0a5d2fd9d339a7a64bd45ff50419fa
aJonathanSchneider <ajs.private@gmail.com>
2019-09-18 14:44:05 +0200

    fixed <video> with autoplaying and muted attribute

--- packages/core/src/components/visualcomposition/videodisplay.tsx
+++ packages/core/src/components/visualcomposition/videodisplay.tsx
@@ -18,8 +18,58 @@ export const VIDEO_IS_AUTOPLAYING = "autoplay";
 export const VIDEO_IS_LOOP = "loop";
 
 let cfgIntrprtKeys: string[] =
-	[LDDict.name, LDDict.fileFormat, LDDict.contentUrl, VIDEO_SHOW_CONTROLS, VIDEO_IS_MUTED, VIDEO_IS_AUTOPLAYING, VIDEO_IS_LOOP, VisualKeysDict.cssClassName];
-let initialKVStores: IKvStore[] = [];
+	[
+		LDDict.name,
+		LDDict.fileFormat,
+		LDDict.contentUrl,
+		VIDEO_SHOW_CONTROLS,
+		VIDEO_IS_MUTED,
+		VIDEO_IS_AUTOPLAYING,
+		VIDEO_IS_LOOP,
+		VisualKeysDict.cssClassName
+	];
+let initialKVStores: IKvStore[] = [
+	{
+		key: LDDict.name,
+		value: undefined,
+		ldType: LDDict.Text
+	},
+	{
+		key: LDDict.fileFormat,
+		value: undefined,
+		ldType: LDDict.Text
+	},
+	{
+		key: LDDict.contentUrl,
+		value: undefined,
+		ldType: LDDict.Text
+	},
+	{
+		key: VIDEO_SHOW_CONTROLS,
+		value: undefined,
+		ldType: LDDict.Boolean
+	},
+	{
+		key: VIDEO_IS_MUTED,
+		value: undefined,
+		ldType: LDDict.Boolean
+	},
+	{
+		key: VIDEO_IS_AUTOPLAYING,
+		value: undefined,
+		ldType: LDDict.Boolean
+	},
+	{
+		key: VIDEO_IS_LOOP,
+		value: undefined,
+		ldType: LDDict.Boolean
+	},
+	{
+		key: VisualKeysDict.cssClassName,
+		value: undefined,
+		ldType: LDDict.Text
+	}
+];
 let bpCfg: BlueprintConfig = {
 	subItptOf: null,
 	canInterpretType: cfgType,
@@ -53,6 +103,8 @@ export class PureVideoDisplay extends Component<LDConnectedState & LDConnectedDi
 	initialKvStores: IKvStore[];
 	consumeLDOptions: (ldOptions: ILDOptions) => any;
 
+	videoRef: HTMLVideoElement;
+
 	constructor(props: any) {
 		super(props);
 		this.cfg = (this.constructor["cfg"] as BlueprintConfig);
@@ -60,6 +112,12 @@ export class PureVideoDisplay extends Component<LDConnectedState & LDConnectedDi
 			[LDDict.name, LDDict.fileFormat, LDDict.contentUrl,
 				VIDEO_SHOW_CONTROLS, VIDEO_IS_MUTED, VIDEO_IS_AUTOPLAYING,
 				VIDEO_IS_LOOP, VisualKeysDict.cssClassName]);
+	}
+
+	componentDidMount() {
+		const { localValues } = this.state;
+		let isMuted = localValues.get(VIDEO_IS_MUTED);
+		this.videoRef.muted = isMuted;
 	}
 
 	render() {
@@ -76,13 +134,19 @@ export class PureVideoDisplay extends Component<LDConnectedState & LDConnectedDi
 		if (!ldOptions) return <div>{TXT_NO_VIDEO}</div>;
 		return <div className={`${CSS_CLASS_NAME} ${cssClassName}`}>
 			<video src={videoLink} className="is-loading"
+				ref={(ref) => this.videoRef = ref}
 				autoPlay={isAutoPlay}
 				muted={isMuted}
 				controls={isShowVideoControls}
 				loop={isLooping}
+				onCanPlay={(ev) => {
+					ev.currentTarget.classList.remove("is-loading");
+					if (isAutoPlay) ev.currentTarget.play();
+				}}
 				onLoad={
 					(ev) => {
 						ev.currentTarget.classList.remove("is-loading");
+						if (isAutoPlay) ev.currentTarget.play();
 					}
 				}>
 			</video>
