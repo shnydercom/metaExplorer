2a4d0a9284b98cd5121c9f2f0a7b4bcbd9d395c3
aJonathanSchneider <ajs.private@gmail.com>
2019-11-06 19:49:23 +0100

    fixed MDSingleImageSelector's file drop

--- mods/material-design/src/components/essentials/content/MDSingleImageSelector.tsx
+++ mods/material-design/src/components/essentials/content/MDSingleImageSelector.tsx
@@ -1,7 +1,11 @@
 import { Button } from "@material-ui/core";
-import { DOMCamera, ldBlueprint, AbstractSingleImageSelector, SingleImageSelectorBpCfg, SingleImageSelectorStateEnum } from "@metaexplorer/core";
-import { default as Dropzone } from "react-dropzone";
-import React from "react";
+import {
+	DOMCamera,
+	ldBlueprint, AbstractSingleImageSelector, SingleImageSelectorBpCfg,
+	SingleImageSelectorStateEnum
+} from "@metaexplorer/core";
+import Dropzone, { DropzoneRef, DropzoneState } from "react-dropzone";
+import React, { createRef, Ref } from "react";
 
 import AddAPhotoIcon from '@material-ui/icons/AddAPhoto';
 
@@ -9,38 +13,51 @@ import AddPhotoAlternate from '@material-ui/icons/AddPhotoAlternate';
 
 @ldBlueprint(SingleImageSelectorBpCfg)
 export class MDSingleImageSelector extends AbstractSingleImageSelector {
+	protected dropzoneRef: Ref<DropzoneRef> = createRef();
 
 	render() {
 		const { curStep, isCamAvailable, previewURL } = this.state;
-		let dropzoneRef;
+		const dzInputKey = "dz-input";
 		return (<Dropzone
 			// className={curStep === SingleImageSelectorStateEnum.isPreviewing ? "single-img-sel accept" : "single-img-sel"}
 			accept="image/*"
 			multiple={false}
+			noClick={true}
 			//disableClick={true}
-			ref={(node) => { dropzoneRef = node; }}
+			ref={this.dropzoneRef}
 			onDropAccepted={(acceptedOrRejected) => {
+				console.log("asdf")
 				let files = acceptedOrRejected.map((file) => ({
 					...file,
 					preview: URL.createObjectURL(file)
 				}));
 				this.onDropSuccess(files[0], files[0].preview);
 			}}
-			onDropRejected={() => this.onDropFailure()}
+			onDropRejected={() => {
+				console.log("fdsa")
+				this.onDropFailure()
+			}}
 			//onDragStart={() => this.startDrag()}
 			onDragEnter={() => this.startDrag()}
 			onDragOver={() => this.startDrag()}
 			onDragLeave={() => this.onDropFailure()}
 			onFileDialogCancel={() => this.onDropFailure()}
+			onDrop={(aF) => console.log(aF)}
 		>
-			{((dzstate) => {
+			{((dzstate: DropzoneState) => {
 				switch (curStep) {
 					case SingleImageSelectorStateEnum.isSelectInputType:
-						return <div className="accept"> {isCamAvailable ? <Button className="btn-extension" onClick={() => { this.startCamera(); }}>
-							<AddAPhotoIcon />
-							Open Camera
-							</Button> : null}
-							<Button className="btn-extension" onClick={() => { dropzoneRef.open(); }}>
+						return <div className="single-img-sel" {...dzstate.getRootProps() as any //todo: check if "as any" can be removed without throwing an error. Currently @types wrong!
+						}>
+							<input key={dzInputKey} {...dzstate.getInputProps() as any //todo: check if "as any" can be removed without throwing an error. Currently @types wrong!
+							} />
+							{isCamAvailable
+								? <Button className="btn-extension" onClick={() => { this.startCamera(); }}>
+									<AddAPhotoIcon />
+									Open Camera
+							</Button>
+								: null}
+							<Button className="btn-extension" onClick={() => { dzstate.open(); }}>
 								<AddPhotoAlternate />
 								Select Image
 								</Button></div>;
@@ -49,7 +66,12 @@ export class MDSingleImageSelector extends AbstractSingleImageSelector {
 							this.onDropSuccess(null, a);
 						}} />;
 					case SingleImageSelectorStateEnum.isDragging:
-						return <div className="accept"><img className="md-large-image" style={{ flex: 1 }} src={this.draggingImgLink} height="100px" /></div>;
+						return <div className="single-img-sel accept" {...dzstate.getRootProps() as any //todo: check if "as any" can be removed without throwing an error. Currently @types wrong!
+						}>
+							<input key={dzInputKey} {...dzstate.getInputProps() as any //todo: check if "as any" can be removed without throwing an error. Currently @types wrong!
+							} />
+							<img className="md-large-image" style={{ flex: 1 }} src={this.draggingImgLink} height="100px" />
+						</div>;
 					case SingleImageSelectorStateEnum.isPreviewing:
 						return <img className="cover-img" src={previewURL} alt="image preview" ></img>;
 					case SingleImageSelectorStateEnum.isError:
