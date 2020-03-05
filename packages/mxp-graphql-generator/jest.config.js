e65c0559b95262ebfa77f37fddd907eed409003a
aJonathanSchneider <ajs.private@gmail.com>
2020-03-05 01:51:45 +0100

    graphql-generator code copied

---
+++ packages/mxp-graphql-generator/jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: process.cwd(),
+  globals: {
+    'ts-jest': {
+      diagnostics: false,
+    },
+  },
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        classNameTemplate: '{classname}',
+        titleTemplate: '{title}',
+        addFileAttribute: 'true',
+      },
+    ],
+  ],
+  modulePathIgnorePatterns: ['<rootDir>/dist'],
+};
