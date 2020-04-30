2c6b2d12cea775210f65ad9a9112886d9cff54a6
aJonathanSchneider <ajs.private@gmail.com>
2020-05-01 00:03:03 +0200

    removed graphql-ts-generator

--- packages/graphql-generator/src/ts-op/ts-operation-variables-to-object.ts
+++
@@ -1,7 +0,0 @@
-import { TypeScriptOperationVariablesToObject as TSOperationVariablesToObject } from '../ts';
-
-export class TypeScriptOperationVariablesToObject extends TSOperationVariablesToObject {
-  protected formatTypeString(fieldType: string, isNonNullType: boolean, hasDefaultValue: boolean): string {
-    return fieldType;
-  }
-}
