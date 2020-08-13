7cc1278e3997f3e52f78e4ba5d059c89843d71e2
jonathanshnyder <contact@metaexplorer.io>
2020-08-13 21:26:16 +0200

    dev-server: calls to save and retrieve globals

--- packages/editor-dev-server/src/editor-to-fs-server.ts
+++ packages/editor-dev-server/src/editor-to-fs-server.ts
@@ -4,15 +4,18 @@ import fs from 'fs';
 import { createBlocksFromLib } from './scripts/create-blocks-from-lib';
 import { mkdir } from './scripts/copydir';
 /*
-	this server will save blocks to the file system, 
+	this server will save blocks to the file system,
 	building a directory structure dependent on the IRI of the "nameSelf"-property.
 	You can specify a basePath that'll be the root of your directory structure.
 	This is a server for development!
 */
 
-const IRI_STYLES = '/styles'
+const IRI_STYLES = '/styles';
 const API_IRI_BLOCKS = '/api/blocks';
+const API_IRI_GLOBALS = '/api/globals';
 const FS_BLOCKS_SUBPATH = 'blocks';
+const FS_BLOCKS_FILENAME = 'interpreters.json';
+const FS_GLOBALS_FILENAME = 'globals.json';
 const REQ_PATH_DETERMINING_KEY = 'nameSelf';
 
 const basePathDefault = './';
@@ -29,34 +32,34 @@ export function editorToFileSystem(basePath?: string, mainItpt?: string, srvrPor
 	}
 
 	let isDirModified = false;
-	let resolvedBasePath = path.resolve(basePath);
-	let resolvedBlockPath = path.join(resolvedBasePath, 'blocks');
+	const resolvedBasePath = path.resolve(basePath);
+	const resolvedBlockPath = path.join(resolvedBasePath, FS_BLOCKS_SUBPATH);
 	fs.mkdirSync(resolvedBasePath, { recursive: true });
 	fs.mkdirSync(resolvedBlockPath, { recursive: true });
 
 	if (!mainItpt) {
 		let indexItpt = {
-			"subItptOf": "c6c6d88d-00e9-4bf2-8c89-c2ebc1b9adbe",
-			"canInterpretType": "my-user/my-project/index-ObjectType",
-			"nameSelf": "my-user/my-project/index",
-			"ownKVLs": [
+			subItptOf: "c6c6d88d-00e9-4bf2-8c89-c2ebc1b9adbe",
+			canInterpretType: "my-user/my-project/index-ObjectType",
+			nameSelf: "my-user/my-project/index",
+			ownKVLs: [
 				{
-					"key": "InterpreterReferenceMapKey",
-					"value": {
+					key: "InterpreterReferenceMapKey",
+					value: {
 						"c6c6d88d-00e9-4bf2-8c89-c2ebc1b9adbe": {
-							"subItptOf": "metaexplorer.io/baseContainer",
-							"canInterpretType": "metaexplorer.io/ContainerObjType",
-							"nameSelf": "c6c6d88d-00e9-4bf2-8c89-c2ebc1b9adbe",
-							"ownKVLs": [],
-							"crudSkills": "cRud",
-							"inKeys": []
+							subItptOf: "metaexplorer.io/baseContainer",
+							canInterpretType: "metaexplorer.io/ContainerObjType",
+							nameSelf: "c6c6d88d-00e9-4bf2-8c89-c2ebc1b9adbe",
+							ownKVLs: [],
+							crudSkills: "cRud",
+							inKeys: []
 						}
 					},
-					"ldType": "InterpreterReferenceMapType"
+					ldType: "InterpreterReferenceMapType"
 				}
 			],
-			"crudSkills": "cRud",
-			"inKeys": []
+			crudSkills: "cRud",
+			inKeys: []
 		};
 		let userName = "my-user";
 		let projectName = "my-project";
@@ -65,7 +68,7 @@ export function editorToFileSystem(basePath?: string, mainItpt?: string, srvrPor
 		mkdir(path.resolve(resolvedBlockPath + "/" + userName));
 		mkdir(path.resolve(resolvedBlockPath + "/" + userName + "/" + projectName));
 		//fs.mkdirSync(path.resolve(resolvedBlockPath + "/" + mainItptPath));
-		fs.writeFileSync(path.join(resolvedBlockPath, mainItpt + ".json"), JSON.stringify(indexItpt))
+		fs.writeFileSync(path.join(resolvedBlockPath, mainItpt + ".json"), JSON.stringify(indexItpt));
 	}
 
 	createBlocksFromLib(mainItpt, resolvedBlockPath, basePath);
@@ -73,22 +76,56 @@ export function editorToFileSystem(basePath?: string, mainItpt?: string, srvrPor
 	const app: express.Application = express();
 	app.use(express.json());
 
-	app.get(IRI_STYLES + "/*", function (req, res) {
-		console.log(req.path)
+	app.get(IRI_STYLES + "/*", (req, res) => {
+		console.log(req.path);
 		let localPath = req.path;
 		localPath = localPath.replace("styles/", "");
-		res.sendFile(path.join(path.resolve(basePath), localPath));
+		res.sendFile(
+			path.join(path.resolve(basePath), localPath),
+			(err) => { throw err; }
+		);
 	});
 
-	app.get(API_IRI_BLOCKS, function (req, res) {
+	/**
+	 * GET call retrieves values to fill in the top-level redux state
+	 */
+	app.get(API_IRI_GLOBALS, (req, res) => {
+		const globalsFilePath = path.join(resolvedBasePath, './' + FS_GLOBALS_FILENAME);
+		if (!fs.existsSync(globalsFilePath)) {
+			fs.writeFileSync(globalsFilePath, JSON.stringify({}));
+		}
+		res.sendFile(globalsFilePath);
+	});
+
+	/**
+	 * POST call saves values from the top-level redux state
+	 */
+	app.post(API_IRI_GLOBALS, (req, res) => {
+		const globalsFilePath = path.join(resolvedBasePath, './' + FS_GLOBALS_FILENAME);
+		fs.writeFileSync(globalsFilePath, JSON.stringify(req.body));
+		res.status(200).json({
+			statusPayload: "saved!",
+			status: 'success'
+		});
+	});
+
+	/**
+	 * GET call retrieves the json of all compound blocks, and builds it from the file
+	 * system if needed
+	 */
+	app.get(API_IRI_BLOCKS, (req, res) => {
 		if (isDirModified) {
-			console.log("recreating from filesystem")
+			console.log("recreating blocks from filesystem");
 			createBlocksFromLib(mainItpt, resolvedBlockPath, basePath);
 		}
 		isDirModified = false;
-		res.sendFile(path.join(resolvedBasePath, './interpreters.json'));
+		res.sendFile(path.join(resolvedBasePath, './' + FS_BLOCKS_FILENAME));
 	});
 
+	/**
+	 * POST call to save a single compound block in the file system, builds a subfolder-structure
+	 * from the block's name
+	 */
 	app.post(API_IRI_BLOCKS, (req, res) => {
 		const reqNameSelf: string = req.body[REQ_PATH_DETERMINING_KEY];
 		let fileName = reqNameSelf;
@@ -107,12 +144,12 @@ export function editorToFileSystem(basePath?: string, mainItpt?: string, srvrPor
 			res.status(200).json({
 				statusPayload: "saved!",
 				status: 'success'
-			})
+			});
 		});
 		isDirModified = true;
 	});
 
-	app.listen(srvrPort, function () {
+	app.listen(srvrPort, () => {
 		console.log(`basePath set to: ${basePath}`);
 		console.log(`Example app listening on port ${srvrPort}!`);
 	});
