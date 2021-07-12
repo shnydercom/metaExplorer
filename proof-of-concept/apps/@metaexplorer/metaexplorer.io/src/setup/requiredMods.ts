ae5a655bbaacefe69221b2b786336d8c840f611c
shnyder <ajs.private@gmail.com>
2021-07-12 20:14:18 +0200

    personalized the page a little bit more

--- proof-of-concept/apps/@metaexplorer/metaexplorer.io/src/setup/requiredMods.ts
+++ proof-of-concept/apps/@metaexplorer/metaexplorer.io/src/setup/requiredMods.ts
@@ -1,10 +1,30 @@
-import { changeMainAppItpt, IModSpec, SingleModStateKeysDict } from '@metaexplorer/core';
-import { MOD_MATERIALDESIGN_ID, initMaterialDesignMod } from '@metaexplorer-mods/material-design';
-import { MOD_USERITPT_ID, initUSERITPTClientMod } from '@metaexplorer-mods/useritpt';
-import { initMetaExplorerMod, MOD_METAEXPLORERIO_ID } from '@metaexplorer-mods/metaexplorer.io';
-import { MOD_QRCODEGENSCAN_ID, initQRCODEGENClientMod } from '@metaexplorer-mods/qr-code-genscan';
-import { isProduction } from '@metaexplorer/core';
-import { initGoogleMod, MOD_GOOGLE_ID } from '@metaexplorer-mods/google';
+import {
+	changeMainAppItpt,
+	IModSpec,
+	SingleModStateKeysDict,
+} from "@metaexplorer/core";
+import {
+	MOD_MATERIALDESIGN_ID,
+	initMaterialDesignMod,
+} from "@metaexplorer-mods/material-design";
+import {
+	MOD_USERITPT_ID,
+	initUSERITPTClientMod,
+} from "@metaexplorer-mods/useritpt";
+import {
+	initMetaExplorerMod,
+	MOD_METAEXPLORERIO_ID,
+} from "@metaexplorer-mods/metaexplorer.io";
+import {
+	MOD_QRCODEGENSCAN_ID,
+	initQRCODEGENClientMod,
+} from "@metaexplorer-mods/qr-code-genscan";
+import {
+	MOD_ITPTEDITOR_ID,
+	initItptEditorMod,
+} from "@metaexplorer-mods/itpt-editor";
+import { isProduction } from "@metaexplorer/core";
+import { initGoogleMod, MOD_GOOGLE_ID } from "@metaexplorer-mods/google";
 
 export function setupRequiredMods(): IModSpec[] {
 	//mod initialization functions
@@ -12,31 +32,33 @@ export function setupRequiredMods(): IModSpec[] {
 	modSpecs.push({
 		id: MOD_QRCODEGENSCAN_ID,
 		initFn: () => initQRCODEGENClientMod(),
-		dependencies: []
-	}
-	);
+		dependencies: [],
+	});
+	modSpecs.push({
+		id: MOD_ITPTEDITOR_ID,
+		initFn: () =>
+			initItptEditorMod(),
+		dependencies: [],
+	});
 	modSpecs.push({
 		id: MOD_MATERIALDESIGN_ID,
 		initFn: () => initMaterialDesignMod(),
-		dependencies: []
-	}
-	);
+		dependencies: [],
+	});
 	modSpecs.push({
 		id: MOD_GOOGLE_ID,
 		initFn: () => initGoogleMod(),
-		dependencies: []
-	}
-	);
+		dependencies: [],
+	});
 	modSpecs.push({
 		id: MOD_USERITPT_ID,
 		initFn: () => initUSERITPTClientMod(isProduction),
-		dependencies: []
-	}
-	);
+		dependencies: [],
+	});
 	modSpecs.push({
 		id: MOD_METAEXPLORERIO_ID,
 		initFn: () => initMetaExplorerMod(),
-		dependencies: [MOD_USERITPT_ID]
+		dependencies: [MOD_USERITPT_ID],
 	});
 	//the final app is also a mod
 	const MOD_NAME = "mxp-init-name";
@@ -50,12 +72,11 @@ export function setupRequiredMods(): IModSpec[] {
 				resolve({
 					id: MOD_ID,
 					name: MOD_NAME,
-					state: SingleModStateKeysDict.readyToUse
+					state: SingleModStateKeysDict.readyToUse,
 				});
 			});
 		},
-		dependencies: [MOD_METAEXPLORERIO_ID]
-	}
-	);
+		dependencies: [MOD_METAEXPLORERIO_ID],
+	});
 	return modSpecs;
 }
